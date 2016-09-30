517053b689a604e9269ddf37e3dd9c9602cf5455
Daniel Patterson <dbp@dbpmail.net>
2016-09-30 14:44:27 -0400

    play around with .gitignore to get the right files committed..

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,15 @@
 set -e
 
 git remote add deploy https://${GH_TOKEN}@github.com:nuprl/nuprl.github.io
-git add .
 git config --global user.email "prl-bot@ccs.neu.edu"
 git config --global user.name "PRL Bot"
+
+grep -v "html" .gitignore > .new-gitignore
+mv .new-gitignore .gitignore
+
+git add .
+git reset HEAD .gitignore
 git commit -e -m "** deploy"
 echo "Committing..."
+
 git push deploy master_test
