6c319b2fb7065cfd769a99da4857a8ab9bff15fc
Daniel Patterson <dbp@dbpmail.net>
2016-09-30 15:10:15 -0400

    be more specific about pushing deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-git remote add deploy https://dbp:${GH_TOKEN}@github.com/nuprl/nuprl.github.io.git
+git remote add deploy https://dbp:$GH_TOKEN@github.com/nuprl/nuprl.github.io.git
 git config --global user.email "prl-bot@ccs.neu.edu"
 git config --global user.name "PRL Bot"
 
@@ -14,4 +14,4 @@ git reset HEAD .gitignore
 git commit -m "$REV"
 echo "Committing..."
 
-git push deploy master_test
+git push deploy HEAD:master_test
