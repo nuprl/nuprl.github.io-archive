5d6a67c2540cb2e2b8c06cf37d17d75119500cc3
Daniel Patterson <dbp@dbpmail.net>
2016-09-30 14:16:13 -0400

    Switch to non-minimal racket; try to build when deploying

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 git remote add deploy https://${GH_TOKEN}@github.com:nuprl/nuprl.github.io
+git checkout -b deploy
+make
 git add .
 git config --global user.email "prl-bot@ccs.neu.edu"
 git config --global user.name "PRL Bot"
--- install-racket.sh
+++ install-racket.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -L -o racket-minimal-6.6-x86_64-linux.sh https://mirror.racket-lang.org/installers/6.6/racket-minimal-6.6-x86_64-linux.sh
-chmod u+rx racket-minimal-6.6-x86_64-linux.sh
-./racket-minimal-6.6-x86_64-linux.sh<<EOF
+curl -L -o racket-6.6-x86_64-linux.sh https://mirror.racket-lang.org/installers/6.6/racket-6.6-x86_64-linux.sh
+chmod u+rx racket-6.6-x86_64-linux.sh
+./racket-6.6-x86_64-linux.sh<<EOF
 no
 "$RACKET_DIR"
 
