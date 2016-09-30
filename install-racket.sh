7d68e6902048f8064c24421c1f09ac152a7e252c
Daniel Patterson <dbp@dbpmail.net>
2016-09-30 13:40:47 -0400

    fix typo in install-racket script

--- install-racket.sh
+++ install-racket.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L -o racket-6.6-x86_64-linux.sh https://mirror.racket-lang.org/installers/6.6/racket-minimal-6.6-x86_64-linux.sh
+curl -L -o racket-minimal-6.6-x86_64-linux.sh https://mirror.racket-lang.org/installers/6.6/racket-minimal-6.6-x86_64-linux.sh
 chmod u+rx racket-minimal-6.6-x86_64-linux.sh
 ./racket-minimal-6.6-x86_64-linux.sh<<EOF
 no
