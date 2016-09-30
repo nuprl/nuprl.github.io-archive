cd3f98c222ff3b5afbc4559e8d82ca8130fec98c
Daniel Patterson <dbp@dbpmail.net>
2016-09-30 14:19:10 -0400

    tweak deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 git remote add deploy https://${GH_TOKEN}@github.com:nuprl/nuprl.github.io
-git checkout -b deploy
-make
 git add .
 git config --global user.email "prl-bot@ccs.neu.edu"
 git config --global user.name "PRL Bot"
