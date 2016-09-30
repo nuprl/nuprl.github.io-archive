bb6c9f0c23bc1b129fee1078e229e19736dd7521
travis@localhost <travis@localhost>
2016-09-30 20:07:14 +0000

    ** deploy nuprl/website@07ec820b44c4067143b90c8bb0c34e5137a069c5

--- deploy.sh
+++ deploy.sh
@@ -11,15 +11,10 @@ git remote add deploy https://github.com/nuprl/nuprl.github.io.git
 git config --global user.email "prl-bot@ccs.neu.edu"
 git config --global user.name "PRL Bot"
 
-grep -v "html" .gitignore > .new-gitignore
-mv .new-gitignore .gitignore
-
 REV="** deploy nuprl/website@$TRAVIS_COMMIT"
-git add .
 git fetch deploy
 git reset --soft deploy/master_test
-git reset HEAD .gitignore
-git checkout HEAD .gitignore
+git add .
 git commit -m "$REV"
 echo "Committing..."
 
